23b107464511880cbc559cb7552b0236395604d0
yesrin <asjs11@naver.com>
2023-06-29 00:43:53 +0900

    login-page.html이 없어서 생기는 오류 해결

--- src/main/java/com/sparta/apiserver1/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/apiserver1/config/WebSecurityConfig.java
@@ -63,14 +63,14 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가 //인증안거쳐도 할수 있음.
                         .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         http.formLogin((formLogin) ->
-                formLogin
-                        .loginPage("/api/user/login-page").permitAll()
+                formLogin.disable()
+//                        .loginPage("/api/user/login-page").permitAll()
         );
 
         // 필터 관리
