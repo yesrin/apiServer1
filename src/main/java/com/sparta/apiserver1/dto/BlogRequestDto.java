63c8581bc31707a376eec8d5ba41b62a98b45672
yesrin <asjs11@naver.com>
2023-06-29 00:46:47 +0900

    username과 password는 pwt로 자동 들어오므로 있을 필요 없음.

--- src/main/java/com/sparta/apiserver1/dto/BlogRequestDto.java
+++ src/main/java/com/sparta/apiserver1/dto/BlogRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class BlogRequestDto {
     private String title;
-    private String username;
-    private String password;
+//    private String username;
+//    private String password;
     private String contents;
 }
